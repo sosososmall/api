6f5b477d53cc7c8625d02beca4519510cee1fc6b
tinker <@qq.com>
2021-01-19 11:30:20 +0800

    delete invalid code

--- src/main/java/com/pokerstar/api/ApiApplication.java
+++ src/main/java/com/pokerstar/api/ApiApplication.java
@@ -3,10 +3,8 @@ package com.pokerstar.api;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
-@EnableTransactionManagement
 @SpringBootApplication
 @MapperScan(basePackages = {"com.pokerstar.api.domain.mapper"})
 public class ApiApplication {
